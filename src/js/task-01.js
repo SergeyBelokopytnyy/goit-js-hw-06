b85bedb841059f4b1cead55cec630394e32a6a1d
Sergey Belokopytnyy <ser.oks.white@gmail.com>
2022-01-13 01:13:45 +0500

    скрипт для решения первого задания

---
+++ src/js/task-01.js
@@ -0,0 +1,7 @@
+const numberCategories = document.querySelectorAll("li.item");
+console.log(`Number of categories: ${numberCategories.length}`);
+
+numberCategories.forEach(function (categories) {
+  console.log(`Category: ${categories.firstElementChild.textContent}`);
+  console.log(`Elements: ${categories.lastElementChild.children.length}`);
+});
