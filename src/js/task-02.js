784ff65ef445e1e9678fb112631e14205925f8d2
Sergey Belokopytnyy <ser.oks.white@gmail.com>
2022-01-16 22:36:08 +0500

    homework solution 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = document.querySelector("ul#ingredients");
+
+ingredients.forEach((element) => {
+  const newItem = document.createElement("li");
+  newItem.textContent = element;
+  newItem.classList.add("item");
+  ingredientsList.append(newItem);
+});
