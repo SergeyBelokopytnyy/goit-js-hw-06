a8402b99daa979341d4928161da50b525ae62d2f
Sergey Belokopytnyy <ser.oks.white@gmail.com>
2022-01-17 15:59:16 +0500

    final update homework solution 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -8,12 +8,11 @@ const ingredients = [
 ];
 
 const ingredientsList = document.querySelector("ul#ingredients");
-const newItemLists = [];
-ingredients.forEach((element) => {
+const newItemList = ingredients.map((element) => {
   const newItem = document.createElement("li");
   newItem.textContent = element;
   newItem.classList.add("item");
-  newItemLists.push(newItem);
-});
 
-ingredientsList.append(...newItemLists);
+  return newItem;
+});
+ingredientsList.append(...newItemList);
