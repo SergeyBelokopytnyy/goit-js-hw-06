e3300d2cdece374d95a5e2ca0ea1518111b8bbb5
Sergey Belokopytnyy <ser.oks.white@gmail.com>
2022-01-19 19:38:45 +0500

    Update homework solution 5

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -1,6 +1,11 @@
 const textInput = document.querySelector("#name-input");
 const textOutput = document.querySelector("#name-output");
 
-textInput.addEventListener("input", (event) => {
+textInput.addEventListener("input", handleInput);
+
+function handleInput(event) {
   textOutput.textContent = event.target.value;
-});
+  if (event.target.value === "") {
+    textOutput.textContent = "Anonymous";
+  }
+}
