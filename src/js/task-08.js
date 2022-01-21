576194e77fc7633269a77beef522772b2c708c1d
Sergey Belokopytnyy <ser.oks.white@gmail.com>
2022-01-22 02:56:48 +0500

    homework solution 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElement = event.currentTarget.elements;
+
+  const mail = formElement.email.value;
+  const password = formElement.password.value;
+
+  if (mail !== "" && password !== "") {
+    const formData = {
+      mail,
+      password,
+    };
+    console.log(formData);
+  } else alert("Все поля должны быть заполнены!");
+
+  event.currentTarget.reset();
+}
