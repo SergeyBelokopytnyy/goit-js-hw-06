6f08e58691a32b038d2a02cc7928f06834fc7eb7
Sergey Belokopytnyy <ser.oks.white@gmail.com>
2022-01-23 01:52:59 +0500

    homework solution 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const bodyEl = document.querySelector("body");
+const btnChangeColor = bodyEl.querySelector("button.change-color");
+const valueChangeColor = bodyEl.querySelector("span.color");
+
+function onChangeColorBody() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  valueChangeColor.textContent = bodyEl.style.backgroundColor;
+}
+
+btnChangeColor.addEventListener("click", onChangeColorBody);
